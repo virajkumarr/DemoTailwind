31cda24f696da453db136638c66a73ec624ae720
virajkumarr <158091034+virajkumarr@users.noreply.github.com>
2025-03-29 10:12:21 +0530

    img

--- src/newcomer.jsx
+++ src/newcomer.jsx
@@ -60,12 +60,12 @@ const Dashboard = () => {
 
         {/* Image Gallery */}
         <div className="grid grid-cols-2 md:grid-cols-3 gap-6 mt-8">
+          <img src="/ltax.jpeg" alt="Tax" className="rounded-lg shadow-md hover:scale-105 transition-transform duration-300" />
           <img src="/tax1.jpeg" alt="Tax" className="rounded-lg shadow-md hover:scale-105 transition-transform duration-300" />
-          <img src="/tax1.jpeg" alt="Tax" className="rounded-lg shadow-md hover:scale-105 transition-transform duration-300" />
-          <img src="/tax1.jpeg" alt="Corporate Tax" className="rounded-lg shadow-md hover:scale-105 transition-transform duration-300" />
-          <img src="/tax4.jpeg" alt="Corporate Tax" className="rounded-lg shadow-md hover:scale-105 transition-transform duration-300" />
           <img src="/tax5.jpeg" alt="Corporate Tax" className="rounded-lg shadow-md hover:scale-105 transition-transform duration-300" />
-          <img src="/ltax.jpeg" alt="Corporate Tax" className="rounded-lg shadow-md hover:scale-105 transition-transform duration-300" />
+          <img src="/x3.jpg" alt="Corporate Tax" className="rounded-lg shadow-md hover:scale-105 transition-transform duration-300" />
+          <img src="/x1.jpg" alt="Corporate Tax" className="rounded-lg shadow-md hover:scale-105 transition-transform duration-300" />
+          <img src="/x2.jpg" alt="Corporate Tax" className="rounded-lg shadow-md hover:scale-105 transition-transform duration-300" />
         </div>
 
       </div>
